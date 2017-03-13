a8e8645f1ecb9c75050e61557243b713e502f379
TKasekamp <toniskasekamp@gmail.com>
2017-03-13 20:55:30 +0200

    eslint conf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-document.body.innerHTML = "<h1>Title added from Javascript</h1>"
+document.body.innerHTML = '<h1>Title added from Javascript</h1>';
 
-document.title = "IFD homework 1"
+document.title = 'IFD homework 1';
