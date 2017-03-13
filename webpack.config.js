5dd1ec9302452ddf834b999e7bb56f307fbe2e79
TKasekamp <toniskasekamp@gmail.com>
2017-03-13 20:05:44 +0200

    Webpack dev server live reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,30 @@ var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/index.js',
-    './styles/app.css'
-  ],
-  output: {
-    filename: 'dist/bundle.js',
-    path: path.join(__dirname, '/'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract('css-loader'),
-      }
+    entry: [
+        './src/index.js',
+        './styles/app.css'
+    ],
+    devServer: {
+        port: 8080
+    },
+    devtool: "inline-source-map",
+    output: {
+        filename: 'dist/bundle.js',
+        path: path.join(__dirname, '/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract('css-loader'),
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'dist/app.css',
+            allChunks: true
+        })
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'dist/app.css',
-      allChunks: true
-    })
-  ]
 };
