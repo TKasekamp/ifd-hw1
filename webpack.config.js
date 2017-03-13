1c3c30d1c0ed1c19e9760bfa67937eab660c3e3f
TKasekamp <toniskasekamp@gmail.com>
2017-03-13 20:42:57 +0200

    Babel added

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract('css-loader'),
+            },
+            {
+                test: /\.js/,
+                use: 'babel-loader'
             }
         ]
     },
