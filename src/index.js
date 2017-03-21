ef2a668f0d80ee8aa7cb1fd3735449bbe7e109b3
TKasekamp <toniskasekamp@gmail.com>
2017-03-21 22:19:04 +0200

    Last result id handling

--- src/index.js
+++ src/index.js
@@ -81,8 +81,8 @@ class GuessForm extends Component {
                         value={this.state.guess}
                         onChange={this.handleAuthorChange.bind(this)}
                     />
-                    <button className='comment-form' onClick={this.onSubmit.bind(this)}>
-                        Submit comment
+                    <button onClick={this.onSubmit.bind(this)}>
+                        Submit guess
                     </button>
 
                 </div>
@@ -101,29 +101,39 @@ class App extends Component {
         this.game = new Game();
 
         this.state = {
-            results: [{guess: 3, id: 1, result: 'equal'}],
+            results: [],
             gameOver: this.game.getGameOver()
         };
     }
 
-    handleCommentSubmit({guess}) {
-        const lastComment = this.state.results[this.state.results.length - 1];
+    handleGuessSubmit({guess}) {
+        const id = this.getLastResultId();
         // Calculate game stuff here
         const result = this.game.makeGuess(guess);
-        this.setState({
-            results: this.state.results.concat({guess, id: lastComment.id + 1, result})
-        });
 
+        this.setState({
+            results: this.state.results.concat({guess, id: id + 1, result})
+        });
         this.setState({
             gameOver: this.game.getGameOver()
         });
+    }
+
+    getLastResultId() {
+        let lastResult = this.state.results[this.state.results.length - 1];
+        if (lastResult === undefined) {
+            return 0;
+        }
+        else {
+            return lastResult.id;
+        }
     }
 
     render() {
         return (
             <div className='app'>
                 <h1>Game lobby</h1>
-                <GuessForm onSubmit={this.handleCommentSubmit.bind(this)} gameOver={this.state.gameOver}/>
+                <GuessForm onSubmit={this.handleGuessSubmit.bind(this)} gameOver={this.state.gameOver}/>
                 <ResultList results={this.state.results}/>
             </div>
         );
