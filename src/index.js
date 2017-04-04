c89ff5ce1f7c8ac23336d4839ec1ab6c3e244f67
TKasekamp <toniskasekamp@gmail.com>
2017-04-04 21:31:24 +0300

    React store added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import App from './containters/App';
+import reducer from './reducers';
+
+let store = createStore(
+    reducer,
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+);
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
