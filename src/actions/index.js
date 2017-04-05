5f1264100f2131f47fca9d437fe30731c5a8d8ff
TKasekamp <toniskasekamp@gmail.com>
2017-04-05 11:04:15 +0300

    jnkjksd

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@
 import {WordGame} from '../WordGame';
 let nextGameId = 1;
 let wordGuessId = 1;
+let numberGuessId = 1;
 let words = ['paper', 'grill', 'basil', 'hinge', 'ruler'];
 
 export const NEW_NUMBER_GAME_CREATED = 'NEW_NUMBER_GAME_CREATED';
@@ -36,30 +37,13 @@ export const wordGuessSubmitted = (guess) => (
     }
 );
 
-// Export action type constants for reducers to use
-export const COMMENT_SUBMITTED = 'COMMENT_SUBMITTED';
-export const commentSubmitted = () => {
-  // An action must be a plain JavaScript object (no instances of classes!)
-  return {
-    type: COMMENT_SUBMITTED,
-    payload: {
-      id: 1
+export const NUMBER_GUESS_SUBMITTED = 'NUMBER_GUESS_SUBMITTED';
+export const numberGuessSubmitted = (guess) => (
+    {
+        type: WORD_GUESS_SUBMITTED,
+        payload:  {
+            guess: guess.guess,
+            id: numberGuessId++
+        }
     }
-  };
-};
-
-export const AUTHOR_SET = 'AUTHOR_SET';
-export const authorSet = (author) => (
-  {
-    type: AUTHOR_SET,
-    payload: author
-  }
 );
-
-// A helper function can be created to create trivial actions and avoid
-// clutter.
-const createPayloadForwardingAction = (type) => (payload) =>
-  ({type: type, payload: payload});
-
-export const TEXT_SET = 'TEXT_SET';
-export const textSet = createPayloadForwardingAction(TEXT_SET);
