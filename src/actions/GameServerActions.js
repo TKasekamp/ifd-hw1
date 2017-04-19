aa8fe4f5feafdd8b090f0862031f64fab0907edd
TKasekamp <toniskasekamp@gmail.com>
2017-04-19 18:27:46 +0300

    Word actions

--- src/actions/GameServerActions.js
+++ src/actions/GameServerActions.js
@@ -2,7 +2,10 @@
  * Created by Tonis on 19.04.2017.
  */
 import jsonAjax from '../JSONAjaxRequest';
-import {newNumberGameCreated, newNumberGameFailed, numberGuessFailed, numberGuessSucceeded} from './index';
+import {
+    newNumberGameCreated, newNumberGameFailed, newWordGameCreated, newWordGameFailed, numberGuessFailed,
+    numberGuessSucceeded, wordGuessFailed, wordGuessSucceeded
+} from './index';
 
 const SERVER_ADDRESS = 'http://localhost:8081';
 
@@ -23,5 +26,25 @@ export const makeNumberGuess = ({id, guess, gameId}) => (dispatch) => {
         {guess},
         ({move, game}) => dispatch(numberGuessSucceeded({id, move, game})),
         ({error} = {}) => dispatch(numberGuessFailed({id, error}))
+    );
+};
+
+export const createWordGame = ({localId, type}) => (dispatch) => {
+    jsonAjax(
+        SERVER_ADDRESS + '/games',
+        'POST',
+        {type},
+        ({id, type, status}) => dispatch(newWordGameCreated({localId, id, type, status})),
+        ({error} = {}) => dispatch(newWordGameFailed({localId, error}))
+    );
+};
+
+export const makeWordGuess = ({id, guess, gameId}) => (dispatch) => {
+    jsonAjax(
+        SERVER_ADDRESS + '/games/' + gameId + '/moves',
+        'POST',
+        {guess},
+        ({move, game}) => dispatch(wordGuessSucceeded({id, move, game})),
+        ({error} = {}) => dispatch(wordGuessFailed({id, error}))
     );
 };
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
-import {WordGame} from '../WordGame';
-import {NumberGame} from '../NumberGame';
 let nextGameId = 0;
 let guessId = 0;
-let words = ['paper', 'grill', 'basil', 'hinge', 'ruler'];
 
 const createPayloadForwardingAction = (type) => (payload) =>
     ({type: type, payload: payload});
+
+// NUMBER GAME
 
 export const NEW_NUMBER_GAME_REQUESTED = 'NEW_NUMBER_GAME_REQUESTED';
 export const newNumberGameRequested = () => (
@@ -18,12 +17,13 @@ export const newNumberGameRequested = () => (
     }
 );
 
-
 export const NEW_NUMBER_GAME_CREATED = 'NEW_NUMBER_GAME_CREATED';
 export const newNumberGameCreated = createPayloadForwardingAction(NEW_NUMBER_GAME_CREATED);
 
 export const NEW_NUMBER_GAME_FAILED = 'NEW_NUMBER_GAME_FAILED';
 export const newNumberGameFailed = createPayloadForwardingAction(NEW_NUMBER_GAME_FAILED);
+
+// NUMBER GAME GUESS
 
 export const NUMBER_GUESS_SUBMITTED = 'NUMBER_GUESS_SUBMITTED';
 export const numberGuessSubmitted = (guess) => (
@@ -37,23 +37,32 @@ export const numberGuessSubmitted = (guess) => (
     }
 );
 
-
 export const NUMBER_GUESS_SUCCEEDED = 'NUMBER_GUESS_SUCCEEDED';
 export const numberGuessSucceeded = createPayloadForwardingAction(NUMBER_GUESS_SUCCEEDED);
 
 export const NUMBER_GUESS_FAILED = 'NUMBER_GAME_FAILED';
 export const numberGuessFailed = createPayloadForwardingAction(NUMBER_GUESS_FAILED);
 
-export const NEW_WORD_GAME_CREATED = 'NEW_WORD_GAME_CREATED';
-export const newWordGameCreated = () => (
+// WORD GAME
+
+export const NEW_WORD_GAME_REQUESTED = 'NEW_WORD_GAME_REQUESTED';
+export const newWordGameRequested = () => (
     {
-        type: NEW_WORD_GAME_CREATED,
+        type: NEW_WORD_GAME_REQUESTED,
         payload: {
-            id: nextGameId++,
-            targetWord: WordGame.selectWord(words)
+            localId: (nextGameId++).toString(),
+            type: 'guess_word'
         }
     }
 );
+
+export const NEW_WORD_GAME_CREATED = 'NEW_WORD_GAME_CREATED';
+export const newWordGameCreated = createPayloadForwardingAction(NEW_WORD_GAME_CREATED);
+
+export const NEW_WORD_GAME_FAILED = 'NEW_WORD_GAME_FAILED';
+export const newWordGameFailed = createPayloadForwardingAction(NEW_WORD_GAME_FAILED);
+
+// WORD GAME GUESS
 
 export const WORD_GUESS_SUBMITTED = 'WORD_GUESS_SUBMITTED';
 export const wordGuessSubmitted = (guess) => (
@@ -67,4 +76,8 @@ export const wordGuessSubmitted = (guess) => (
     }
 );
 
+export const WORD_GUESS_SUCCEEDED = 'WORD_GUESS_SUCCEEDED';
+export const wordGuessSucceeded = createPayloadForwardingAction(WORD_GUESS_SUCCEEDED);
 
+export const WORD_GUESS_FAILED = 'WORD_GAME_FAILED';
+export const wordGuessFailed = createPayloadForwardingAction(WORD_GUESS_FAILED);
