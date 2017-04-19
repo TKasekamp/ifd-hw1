53a57f7b11714a6e40f131e6a6d0e0ea3ccf40b1
TKasekamp <toniskasekamp@gmail.com>
2017-04-19 18:29:15 +0300

    Middleware

--- src/middlewares/GameServerMiddleware.js
+++ src/middlewares/GameServerMiddleware.js
@@ -1,10 +1,23 @@
-import {NEW_NUMBER_GAME_REQUESTED, NUMBER_GUESS_SUBMITTED} from '../actions/index';
-import {createNewGame, createNumberGame, makeNumberGuess} from '../actions/GameServerActions';
+import {
+    NEW_NUMBER_GAME_REQUESTED,
+    NEW_WORD_GAME_REQUESTED,
+    NUMBER_GUESS_SUBMITTED,
+    WORD_GUESS_SUBMITTED
+} from '../actions/index';
+import {
+    createNumberGame,
+    createWordGame,
+    makeNumberGuess,
+    makeWordGuess
+} from '../actions/GameServerActions';
 
 const ACTION_TYPE_TO_SERVER_ACTION = {
-    [NEW_NUMBER_GAME_REQUESTED]: createNumberGame,
-    [NUMBER_GUESS_SUBMITTED]: makeNumberGuess
-};
+        [NEW_NUMBER_GAME_REQUESTED]: createNumberGame,
+        [NEW_WORD_GAME_REQUESTED]: createWordGame,
+        [NUMBER_GUESS_SUBMITTED]: makeNumberGuess,
+        [WORD_GUESS_SUBMITTED]: makeWordGuess
+    }
+;
 
 const gameServerMiddleware = (store) => (next) => (action) => {
     const serverAction = ACTION_TYPE_TO_SERVER_ACTION[action.type];
