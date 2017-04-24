1cb4015a11d40796bb5f2ef0136d074f19686f6f
TKasekamp <toniskasekamp@gmail.com>
2017-04-24 16:31:24 +0300

    Reducer working somewhat

--- src/reducers/Players.js
+++ src/reducers/Players.js
@@ -1,14 +1,42 @@
+import {MESSAGE_RECEIVED} from '../actions/PlayerActions';
+
 const initialState = {
     connected: false,
-    player: {},
+    playerId: '',
     onlinePlayers: []
 };
 
+const CONNECTION_ACCEPTED = 'connection:accepted';
+const ONLINE_PLAYERS = 'online-players';
+
 const players = (state = initialState, action) => {
     switch (action.type) {
+        case MESSAGE_RECEIVED:
+            if (action.payload.eventName === CONNECTION_ACCEPTED) {
+                return connectionAccepted(state, action);
+            }
+            else if (action.payload.eventName === ONLINE_PLAYERS) {
+                return onlinePlayers(state, action);
+            }
+            return state;
         default:
             return state;
     }
+};
+
+const connectionAccepted = (state, action) => {
+    return {
+        ...state,
+        connected: true,
+        playerId: action.payload.payload.playerId
+    };
+};
+
+const onlinePlayers = (state, action) => {
+    return {
+        ...state,
+        onlinePlayers: action.payload.payload
+    };
 };
 
 export default players;
