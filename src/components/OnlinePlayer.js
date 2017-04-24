d4590da9f8198b114cb1b5e0894c60802e4c8835
TKasekamp <toniskasekamp@gmail.com>
2017-04-24 16:53:16 +0300

    Player names component

---
+++ src/components/OnlinePlayer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+/*
+ OnlinePlayer knows how to display itself.
+ */
+const OnlinePlayer = (props) => {
+    let text = <div>{props.name}</div>;
+
+    if (props.isPlayer) {
+        text = <div>{props.name} (you)</div>
+    }
+
+    return (
+        <div>
+            {text}
+        </div>
+    );
+};
+OnlinePlayer.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    isPlayer: React.PropTypes.bool.isRequired
+};
+
+export default OnlinePlayer;
---
+++ src/components/OnlinePlayerList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import NumberResult from './NumberResult';
+import OnlinePlayer from './OnlinePlayer';
+
+const OnlinePlayerList = (props) => {
+    const players = props.onlinePlayers.map((player) => {
+        return (
+            <OnlinePlayer name={player.name} isPlayer={props.playerId === player.id} key={player.id}>
+            </OnlinePlayer>
+        );
+    });
+    return (
+        <div>
+            <h3>Online players</h3>
+            <div className='flexbox-container result-list'>{players}</div>
+        </div>
+    );
+};
+
+OnlinePlayerList.propTypes = {
+    playerId: React.PropTypes.string.isRequired,
+    onlinePlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired,
+        id: React.PropTypes.number.isRequired,
+    }).isRequired).isRequired
+};
+
+export default OnlinePlayerList;
