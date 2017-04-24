2792b64a4171b70625614650be2ca231f94c5586
TKasekamp <toniskasekamp@gmail.com>
2017-04-24 18:02:28 +0300

    Connect form also has disconnect button

--- src/components/ConnectForm.js
+++ src/components/ConnectForm.js
@@ -13,34 +13,40 @@ class ConnectForm extends Component {
     }
 
     onConnect() {
-        this.props.onConnect({playerName: this.state.playerName});
-        this.setState({playerName: ''});
-    }
-
-    handleKeyPress(e) {
-        if (e.key === 'Enter' && this.state.playerName != '') {
-            this.onConnect();
+        if (this.state.playerName != '') {
+            this.props.onConnect({playerName: this.state.playerName});
+            this.setState({playerName: ''});
         }
     }
 
     render() {
-        return (
-            <div >
-                <input
-                    id="name-input"
-                    type="text"
-                    placeholder="Enter name"
-                    value={this.state.playerName}
-                    onChange={this.handleNameChange.bind(this)}
-                    onKeyPress={this.handleKeyPress.bind(this)}
-                />
-            </div>
-        );
-
+        if (this.props.connected) {
+            return <button type='submit' onClick={this.props.onDisconnect}>
+                Disconnect
+            </button>;
+        }
+        else {
+            return (
+                <div >
+                    <input
+                        id="name-input"
+                        type="text"
+                        placeholder="Enter name"
+                        value={this.state.playerName}
+                        onChange={this.handleNameChange.bind(this)}
+                    />
+                    <button type='submit' onClick={this.onConnect.bind(this)}>
+                        Connect
+                    </button>
+                </div>
+            );
+        }
     }
 }
 ConnectForm.propTypes = {
-    onConnect: React.PropTypes.func.isRequired
+    onConnect: React.PropTypes.func.isRequired,
+    onDisconnect: React.PropTypes.func.isRequired,
+    connected: React.PropTypes.bool.isRequired,
 };
 
 export default ConnectForm;
---
+++ test/components/ConnectFormTest.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ConnectForm from '../../src/components/ConnectForm';
+
+describe('ConnectForm', () => {
+    it('renders', () => {
+        expect(shallow(
+            <ConnectForm onConnect={sinon.stub()} onDisconnect={sinon.stub()} connected={false}/>
+        )).to.exist;
+    });
+
+    it('submits guess when enter pressed', () => {
+        const onSubmit = sinon.stub();
+        const form = shallow(<ConnectForm onConnect={onSubmit} onDisconnect={sinon.stub()} connected={false}/>);
+
+        form.setState({playerName: 'aaa'});
+
+        form.find('button').simulate('click');
+
+        expect(onSubmit).to.have.been.calledWith({playerName: 'aaa'});
+    });
+
+    it('no call when no input', () => {
+        const onSubmit = sinon.stub();
+        const form = shallow(<ConnectForm onConnect={onSubmit} onDisconnect={sinon.stub()} connected={false}/>);
+
+        form.setState({playerName: ''});
+
+        form.find('button').simulate('click');
+
+        expect(onSubmit).to.not.have.been.calledWith({playerName: ''});
+    });
+
+    it('clears state when submit button clicked', () => {
+        const form = shallow(<ConnectForm onConnect={sinon.stub()} onDisconnect={sinon.stub()} connected={false}/>);
+
+        form.setState({playerName: '2'});
+        form.find('button').simulate('click');
+        expect(form.state()).to.eql({playerName: ''});
+    });
+
+    it('renders disconnect button with action', () => {
+        const onSubmit = sinon.stub();
+        const form = shallow(<ConnectForm onDisconnect={onSubmit} onConnect={sinon.stub()} connected={true}/>);
+
+        form.find('button').simulate('click');
+
+        expect(onSubmit).to.have.been.calledWith();
+    });
+});
