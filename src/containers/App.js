3f074edaee00fae666f616df12b87be3d1bbd227
TKasekamp <toniskasekamp@gmail.com>
2017-05-03 10:50:04 +0300

    React routing code basic stuff

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import GamesContainer from './GamesContainer';
 import HeaderContainer from './HeaderContainer';
+import {ConnectedRouter} from 'connected-react-router';
 
 class App extends Component {
     constructor(props) {
@@ -9,13 +10,19 @@ class App extends Component {
 
     render() {
         return (
+            <ConnectedRouter history={this.props.history}>
             <div className='app'>
                 <h1>Game lobby</h1>
                 <HeaderContainer/>
                 <GamesContainer />
             </div>
+            </ConnectedRouter>
         );
     }
 }
+
+App.propTypes = {
+    history: React.PropTypes.object.isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@ import gameServerMiddleware from './middlewares/GameServerMiddleware';
 import webSocketMiddleware from './middlewares/WebSocketMiddleware';
 import App from './containers/App';
 import reducer from './reducers';
+import {createBrowserHistory} from 'history';
+import {connectRouter, routerMiddleware} from 'connected-react-router';
 
 const composeStoreEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Create a abstraction over DOM history API which will be passed both to React
+// Router and Redux.
+const history = createBrowserHistory();
+
 let store = createStore(
-    reducer,
+    connectRouter(history)(reducer),
     composeStoreEnhancers(
         applyMiddleware(
+            routerMiddleware(history), // Integrate history actions with history API
             thunk,
             gameServerMiddleware,
             webSocketMiddleware
@@ -24,7 +31,7 @@ let store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <App history={history}/>
     </Provider>,
     document.getElementById('root')
 );
