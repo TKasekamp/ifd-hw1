3610563152b85af197076b9b406640d6cafd6bb4
TKasekamp <toniskasekamp@gmail.com>
2017-05-04 13:46:31 +0300

    Home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,18 @@ import GamesContainer from '../containers/GamesContainer';
 const HomePage = () => {
     return <div>
         <h1>Game lobby</h1>
-        This is the best game lobby! Details from the above menu.
+        <div style={{width: 600 + 'px'}}>
+            <p>
+                This is the best game lobby! Details from the above menu.
+            </p>
+            <p>
+                Everything from HW6 works on this page.
+            </p>
+            <p>
+                The player connect input is only here as I didn't notice the GIF at the end of the lecture until
+                everything was finished and I don't want to change stuff again.
+            </p>
+        </div>
         <HeaderContainer />
         <GamesContainer/>
     </div>;
